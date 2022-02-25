6e8ef942c81a7a6120080c3054d0e1f0557ec9ad
Robert Clausecker <fuz@fuz.su>
2022-02-25 19:18:55 +0100

    benchmark/golike: implement performance counters
    Only instruction and cycle count have been implemented
    so far, but it's easy to add additional counters if desired.

--- benchmark/golike/benchmark.c
+++ benchmark/golike/benchmark.c
@@ -6,6 +6,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+
+#include <linux/perf_event.h>
+#include <linux/hw_breakpoint.h>
+#include <sys/syscall.h>
 
 #if FLAGSIZE == 64
 typedef uint64_t flags_type;
@@ -35,7 +40,7 @@ pospopcnt_count16avx512(const uint16_t *data, uint32_t len, flags_type *flags)
 	count16avx512(flags, data, len);
 }
 
-static struct pospopcnt_u16_method {
+static const struct pospopcnt_u16_method {
 	const char *name;
 	pospopcnt_u16 *method;
 } methods[] = {
@@ -47,23 +52,74 @@ static struct pospopcnt_u16_method {
 	{ NULL, NULL },
 };
 
+static int event_group = -1, num_counters = 0;
+enum { EVENT_COUNT = 2 };
+static struct event {
+	uint32_t type;
+	int fd;
+	uint64_t conf;
+} events[EVENT_COUNT] = {
+	{ PERF_TYPE_HARDWARE, -1, PERF_COUNT_HW_CPU_CYCLES },
+	{ PERF_TYPE_HARDWARE, -1, PERF_COUNT_HW_INSTRUCTIONS },
+};
+
+/* set up performance counters so performance measurements can be taken */
+static void init_counters()
+{
+	int i;
+	struct perf_event_attr attribs;
+
+	memset(&attribs, 0, sizeof attribs);
+	attribs.exclude_kernel = 1;
+	attribs.exclude_hv = 1;
+	attribs.sample_period = 0;
+	attribs.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_ID;
+
+	for (i = 0; i < EVENT_COUNT; i++) {
+		attribs.type = events[i].type;
+		attribs.config = events[i].conf;
+		events[i].fd = syscall(SYS_perf_event_open, &attribs, 0, -1, event_group, 0);
+		if (events[i].fd == -1) {
+			perror("perf_event_open");
+			continue;
+		}
+
+		num_counters++;
+
+		if (event_group == -1)
+			event_group = events[i].fd;
+	}
+}
+
 /* state of performance counters at one point in time */
 struct counters {
 	struct timespec ts;
+	uint64_t counters[2 * EVENT_COUNT + 1];
 };
 
 typedef int testfunc(struct counters *c, void *payload, size_t n, size_t m);
+
+/* initialise hardware perf counters */
 
 /* set counters to their current value */
 static int
 reset_counters(struct counters *c)
 {
+	ssize_t count;
 	int res;
 
 	res = clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &c->ts);
 	if (res == -1) {
 		perror("clock_gettime");
 		return (-1);
+	}
+
+	if (event_group != -1) {
+		count = read(event_group, c->counters, (2 * num_counters + 1) * sizeof c->counters[0]);
+		if (count < 0) {
+			perror("read(event_group, ...)");
+			return (-1);
+		}
 	}
 
 	return (0);
@@ -86,6 +142,22 @@ tsdiff(struct timespec start, struct timespec end)
 	return (sec + nsec * 1.0e-9);
 }
 
+/* compute the difference between the two counter vectors */
+static void
+counterdiff(uint64_t out[EVENT_COUNT], uint64_t start[], uint64_t end[])
+{
+	int i, j;
+
+	for (i, j = 0; i < EVENT_COUNT; i++) {
+		if (events[i].fd == -1)
+			out[i] = 0;
+		else {
+			out[i] = end[2*j+1] - start[2*j+1];
+			j++;
+		}
+	}
+}
+
 /* print test results */
 /* https://golang.org/design/14313-benchmark-format */
 static void
@@ -93,14 +165,26 @@ print_results(
     const char *name,
     struct counters *start, struct counters *end,
     size_t n, size_t m) {
+	uint64_t counts[EVENT_COUNT];
 	double elapsed;
 
 	elapsed = tsdiff(start->ts, end->ts);
+	counterdiff(counts, start->counters, end->counters);
 
 	if (name == NULL || name[0] == '\0')
 		name = " ";
-	printf("Benchmark%c%s/%zuB\t%10zu\t%.8g ns/op\t%.8g MB/s\n",
-	    toupper(name[0]), name+1, n, m, (elapsed * 1e9) / m, (n * m * 1e-6) / elapsed);
+
+	printf("Benchmark%c%s/%zuB\t%10zu\t"
+	    "%.8g ns/op\t%.8g MB/s",
+	    toupper(name[0]), name+1, n, m,
+	    (elapsed * 1e9) / m, (1e-6 * n * m) / elapsed);
+
+	if (num_counters == EVENT_COUNT) {
+		printf("\t%.8g cy/B\t%.8g ins/B\t%.8g ipc\n",
+		    counts[0]/((double)n * m), counts[1]/((double)n * m),
+		    (double)counts[1] / counts[0]);
+	} else
+		putchar('\n');
 }
 
 /* run one test case for the specified n and print the result */
@@ -212,6 +296,7 @@ main(int argc, char *argv[])
 	char *fake_argv[] = { argv[0], "100000", NULL };
 
 	setlinebuf(stdout);
+	init_counters();
 
 	if (argc < 2) {
 		argv = fake_argv;
